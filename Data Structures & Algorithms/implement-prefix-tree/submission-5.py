546e1d044530f46ecea2649859b25bd4a19d70fe
Ayush Gupta <in.ayushgupta@gmail.com>
2026-04-20 15:16:31 +0530

    Add: implement-prefix-tree - submission-5

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-5.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, letter):
+        self.letter = letter
+        self.ending = False
+        self.pointers = [None] * 26
+        
+def letter_index(letter: str) -> int:
+    return ord(letter) - ord('a')
+
+class PrefixTree:
+
+    def __init__(self):
+        self.trie = Node(' ')
+
+    def insert(self, word: str) -> None:
+
+        curr = self.trie
+
+        for letter in word:
+            index = letter_index(letter)
+            if not curr.pointers[index]:
+                curr.pointers[index] = Node(letter)
+            curr = curr.pointers[index]
+        
+        curr.ending = True
+            
+
+    def search(self, word: str) -> bool:
+
+        curr = self.trie
+
+        for letter in word:
+            index = letter_index(letter)
+            if not curr.pointers[index]:
+                return False
+            curr = curr.pointers[index]
+        
+        return curr.ending
+        
+
+    def startsWith(self, prefix: str) -> bool:
+
+        curr = self.trie
+        
+        for letter in prefix:
+            index = letter_index(letter)
+            if not curr.pointers[index]:
+                return False
+            curr = curr.pointers[index]
+        
+        return True
+        
+        
